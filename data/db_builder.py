bc1e5977a9c8ce548a09bab2fd18c02c8db2b8fa
Haley Zeng <hzeng@stuy.edu>
2017-05-16 14:02:55 -0400

    wjfo

--- data/db_builder.py
+++ data/db_builder.py
@@ -2,21 +2,24 @@ from pymongo import MongoClient
 import csv
 
 server = MongoClient("lisa.stuy.edu")
-db = server['platekschengshi']
+db = server['ttpp']
 
 def initialize():
     init_courses()
-    init_students()
+  #  init_students()
 
 def init_courses():
     f = open("courses.csv")
     course_list = csv.DictReader(f)
     for elem in course_list:
+        print elem
         course = {}
         course["code"] = elem["CourseCode"]
         course["name"] = elem["CourseName"]
         course["department"] = elem["Department"]
-        
+        course["prereq_courses"] = []
+        course["prereq_overall_average"] = 0
+        course["prereq_department_average"] = 0
         db.courses.insert_one(course)
 
 def init_students():
