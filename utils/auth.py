e562b63c56a02c98f78516a6e79ab82a5eaf9d2a
oreo21 <reok99@gmail.com>
2017-05-17 14:04:09 -0400

    converted flask app from SQLite to MongoDB

--- utils/auth.py
+++ utils/auth.py
@@ -1,4 +1,9 @@
+from pymongo import MongoClient
 import hashlib, sqlite3
+
+server = MongoClient()
+# c = MongoClient('lisa.stuy.edu')
+ourDB = server['database'] 
 
 def authenticate(data):
     if data[2] == 'Register':
@@ -9,7 +14,42 @@ def authenticate(data):
 def hashPass(password):
     return hashlib.sha512(password).hexdigest()
 
-def userExists(username, c):
+def userExists(username):
+    #return bool(ourDB.users.find_one({"username": username}))
+    cursor = ourDB.users.find()
+    for user in cursor:
+        if username == user['username']:
+            return True
+    return False
+
+def register(user, password):
+    result = []
+    if (userExists(user)):
+        result = ['User already exists.', False]
+    elif not user.isalnum() or not password.isalnum():
+        result = ['Username and password may only consist of alphanumeric characters.', False]
+    else:
+        entry = {}
+        entry['username'] = user
+        entry['password'] = hashPass(password)
+        ourDB.users.insert_one(entry)
+        result =['Registration successful.', False]
+    return result
+
+def login(user, password):
+    result = []
+    if not userExists(user):
+        result = ['User does not exist.', False]
+    else:
+        p = ourDB.users.find_one({"username": user})['password']
+        if (p != hashPass(password)):
+            result = ['Incorrect password.', False]
+        else:
+            result = ['Login successful.', True]
+    return result
+
+
+def userExistsSQL(username, c):
     s = c.execute("SELECT name FROM users")
     for r in s:
         name = r[0]
@@ -17,11 +57,11 @@ def userExists(username, c):
             return True
     return False
 
-def register(user, password):
+def registerSQL(user, password):
     result = []
     bd = sqlite3.connect('data/bd.db')
     c = bd.cursor()
-    if (userExists(user, c)):
+    if (userExistsSQL(user, c)):
         result = ['User already exists.', False]
     elif not user.isalnum() or not password.isalnum():
         result = ['Username and password may only consist of alphanumeric characters.', False]
@@ -33,11 +73,11 @@ def register(user, password):
         result = ['Registration successful.', False]
     return result
 
-def login(user, password):
+def loginSQL(user, password):
     result = []
     bd = sqlite3.connect('data/bd.db')
     c = bd.cursor()
-    if (userExists(user, c) == False):
+    if (userExistsSQL(user, c) == False):
         result = ['User does not exist.', False]
     else:
         s = c.execute("SELECT password FROM users WHERE name = '%s'"%(user))
