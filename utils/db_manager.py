2c5d0747a8fd9a5d9687a734b924f2a57098f203
Haley Zeng <hzeng@stuy.edu>
2017-05-23 23:02:21 -0400

    wip

--- utils/db_manager.py
+++ utils/db_manager.py
@@ -15,23 +15,39 @@ def list_departments():
     return ret
 
 def init_students(f):
-    seen = []
-    student_list = csv.DictReader(f)
-    for elem in student_list:
-        print elem
-        if elem["StudentID"] not in seen:
+    seen = {}
+    transcript = csv.DictReader(f)
+    for class_record in transcript:
+        
+        course_code = class_record["Course"]
+        course_dept = db.courses.find_one( {"code": course_code } )["department"]
+
+        if class_record["StudentID"] not in seen:
             student = {}
-            student['id'] = elem["StudentID"]
-            seen.append(elem["StudentID"])
-            student['first_name'] = elem["FirstName"]
-            student['last_name'] = elem["LastName"]
-            student['cohort'] = elem["Year"]
+            student['id'] = class_record["StudentID"]
+            seen.append(class_record["StudentID"])
+            student['first_name'] = class_record["FirstName"]
+            student['last_name'] = class_record["LastName"]
+            student['cohort'] = class_record["Year"]
+            
             student['classes_taken'] = {}
             student['department_averages'] = {}
             depts = list_departments()
             for dept in depts:
                 student['classes_taken'][dept] = []
                 student['department_averages'][dept] = 0
+
             student['overall_average'] = 0
             student['selections'] = []
-            db.students.insert_one(student)
+            seen[class_record["StudentID"]] = student
+
+        else:
+            student = seen[class_record["StudentID"]]
+        student["classes_taken"][course_dept].append(course_code)
+
+        data = []
+        for entry in seen:
+            data.append( seen[entry] )
+        print data
+        
+        db.students.insert_many(data)
